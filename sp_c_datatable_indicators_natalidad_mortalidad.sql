742d4a4615e705c3b36b6a5f49ef6d23978dc3c2
Marcelino <m.jorge.romero@gmail.com>
2017-07-11 02:46:54 -0400

    Stored Procedure V3

--- sp_c_datatable_indicators_natalidad_mortalidad.sql
+++ sp_c_datatable_indicators_natalidad_mortalidad.sql
@@ -34,62 +34,84 @@ BEGIN
 	--SET @end_date = 1973;
 
 	SELECT
-		Indicators.CountryName, Indicators.CountryCode, Indicators.IndicatorCode, Indicators.Year, Indicators.Value
-	INTO #IndicatorsPartial
-	FROM dbo.Indicators AS Indicators
-	WHERE Indicators.IndicatorCode IN ('SP.DYN.CBRT.IN', 'SP.DYN.AMRT.MA', 'SP.DYN.AMRT.FE', 'SP.DYN.IMRT.IN', 'SH.DYN.MORT')
-
-
-	;WITH IndicatorsCountries AS
-	(
-		SELECT DISTINCT
-			CountryName,
-			CountryCode,
-			Year
-		FROM #IndicatorsPartial
-	)
-	SELECT
-		IndicatorsCountries.CountryName,
-		IndicatorsCountries.CountryCode,
-		IndicatorsCountries.Year,
-		BirthRateCrudeTb.Value AS BirthRateCrude,
-		MortalityRateAdultMaleTb.Value AS MortalityRateAdultMale,
-		MortalityRateAdultFemaleTb.Value AS MortalityRateAdultFemale,
-		MortalityRateInfantTb.Value AS MortalityRateInfant,
-		MortalityRateUnder5Tb.Value AS MortalityRateUnder5,
-		(MortalityRateAdultMaleTb.Value + MortalityRateAdultFemaleTb.Value + MortalityRateInfantTb.Value + MortalityRateUnder5Tb.Value) AS MortalityRate
-	INTO #IndicatorsComplete
-	FROM IndicatorsCountries
-	JOIN #IndicatorsPartial AS BirthRateCrudeTb
-		ON BirthRateCrudeTb.CountryCode = IndicatorsCountries.CountryCode
-		AND BirthRateCrudeTb.Year = IndicatorsCountries.Year
-	JOIN #IndicatorsPartial AS MortalityRateAdultMaleTb
-		ON MortalityRateAdultMaleTb.CountryCode = BirthRateCrudeTb.CountryCode
-		AND MortalityRateAdultMaleTb.Year = IndicatorsCountries.Year
-	JOIN #IndicatorsPartial AS MortalityRateAdultFemaleTb
-		ON MortalityRateAdultFemaleTb.CountryCode = IndicatorsCountries.CountryCode
-		AND MortalityRateAdultFemaleTb.Year = IndicatorsCountries.Year
-	JOIN #IndicatorsPartial AS MortalityRateInfantTb
-		ON MortalityRateInfantTb.CountryCode = IndicatorsCountries.CountryCode
-		AND MortalityRateInfantTb.Year = IndicatorsCountries.Year
-	JOIN #IndicatorsPartial AS MortalityRateUnder5Tb
-		ON MortalityRateUnder5Tb.CountryCode = IndicatorsCountries.CountryCode
-		AND MortalityRateUnder5Tb.Year = IndicatorsCountries.Year
+		CountryName, CountryCode, IndicatorCode, Year, Value
+	INTO #IndicatorsRaw
+	FROM dbo.Indicators
 	WHERE
-		BirthRateCrudeTb.IndicatorCode = 'SP.DYN.CBRT.IN'
-		AND MortalityRateAdultMaleTb.IndicatorCode = 'SP.DYN.AMRT.MA'
-		AND MortalityRateAdultFemaleTb.IndicatorCode = 'SP.DYN.AMRT.FE'
-		AND MortalityRateInfantTb.IndicatorCode = 'SP.DYN.IMRT.IN'
-		AND MortalityRateUnder5Tb.IndicatorCode = 'SH.DYN.MORT'
-		AND (@country_code IS NULL OR IndicatorsCountries.CountryCode = @country_code)
+		IndicatorCode IN ('SP.DYN.CBRT.IN', 'SP.DYN.AMRT.MA', 'SP.DYN.AMRT.FE', 'SP.DYN.IMRT.IN', 'SH.DYN.MORT')
+		AND (@country_code IS NULL OR CountryCode = @country_code)
 		AND (
 			(@start_date IS NULL AND @end_date IS NULL) OR
-			(@start_date IS NOT NULL AND @end_date IS NOT NULL AND IndicatorsCountries.Year BETWEEN @start_date AND @end_date) OR
-			(@start_date IS NULL AND @end_date IS NOT NULL AND IndicatorsCountries.Year <= @end_date) OR
-			(@start_date IS NOT NULL AND @end_date IS NULL AND IndicatorsCountries.Year >= @start_date)
+			(@start_date IS NOT NULL AND @end_date IS NOT NULL AND Year BETWEEN @start_date AND @end_date) OR
+			(@start_date IS NULL AND @end_date IS NOT NULL AND Year <= @end_date) OR
+			(@start_date IS NOT NULL AND @end_date IS NULL AND Year >= @start_date)
 		)
-	
-	DROP TABLE #IndicatorsPartial
+
+	-- Transformación de la tabla vertical anterior a horizontal
+	;WITH IndicatorCountries AS
+	(
+		SELECT DISTINCT
+			CountryName, CountryCode, Year
+		FROM #IndicatorsRaw
+	)
+	SELECT
+		IndicatorCountries.CountryName,
+		IndicatorCountries.CountryCode,
+		IndicatorCountries.Year,
+		BirthRateCrudeTb.BirthRateCrude,
+		MortalityRateAdultMaleTb.MortalityRateAdultMale,
+		MortalityRateAdultFemaleTb.MortalityRateAdultFemale,
+		MortalityRateInfantTb.MortalityRateInfant,
+		MortalityRateUnder5Tb.MortalityRateUnder5,
+		(MortalityRateAdultMaleTb.MortalityRateAdultMale + MortalityRateAdultFemaleTb.MortalityRateAdultFemale + 
+		MortalityRateInfantTb.MortalityRateInfant + MortalityRateUnder5Tb.MortalityRateUnder5) AS MortalityRate
+	INTO #IndicatorsComplete
+	FROM IndicatorCountries
+	CROSS APPLY
+	(
+		SELECT T.Value AS BirthRateCrude FROM #IndicatorsRaw AS T
+		WHERE
+			T.IndicatorCode = 'SP.DYN.CBRT.IN'
+			AND T.CountryCode = IndicatorCountries.CountryCode
+			AND T.Year = IndicatorCountries.Year
+	) AS BirthRateCrudeTb
+	CROSS APPLY
+	(
+		SELECT T.Value AS MortalityRateAdultMale FROM #IndicatorsRaw AS T
+		WHERE
+			T.IndicatorCode = 'SP.DYN.AMRT.MA'
+			AND T.CountryCode = IndicatorCountries.CountryCode
+			AND T.Year = IndicatorCountries.Year
+	) AS MortalityRateAdultMaleTb
+	CROSS APPLY
+	(
+		SELECT T.Value AS MortalityRateAdultFemale FROM #IndicatorsRaw AS T
+		WHERE
+			T.IndicatorCode = 'SP.DYN.AMRT.FE'
+			AND T.CountryCode = IndicatorCountries.CountryCode
+			AND T.Year = IndicatorCountries.Year
+
+	) AS MortalityRateAdultFemaleTb
+	CROSS APPLY
+	(
+		SELECT T.Value AS MortalityRateInfant FROM #IndicatorsRaw AS T
+		WHERE
+			T.IndicatorCode = 'SP.DYN.IMRT.IN'
+			AND T.CountryCode = IndicatorCountries.CountryCode
+			AND T.Year = IndicatorCountries.Year
+
+	) AS MortalityRateInfantTb
+	CROSS APPLY
+	(
+		SELECT T.Value AS MortalityRateUnder5 FROM #IndicatorsRaw AS T
+		WHERE
+			T.IndicatorCode = 'SH.DYN.MORT'
+			AND T.CountryCode = IndicatorCountries.CountryCode
+			AND T.Year = IndicatorCountries.Year
+
+	) AS MortalityRateUnder5Tb
+
+	DROP TABLE #IndicatorsRaw
 
 	SELECT @total = COUNT(*) FROM #IndicatorsComplete;
 
